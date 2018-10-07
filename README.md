eaf20be1b7e1b847cb45dc45df70cecffef9b409
Jonathan Xu <jonathanj.xu97@gmail.com>
2018-10-07 13:56:22 -0500

    First draft for splash page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# site-2019
+# EOH Website 2019
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import logo from './assets/logo-white.png';
+import './App.scss';
+
+const wordList = [
+  'Boundaries',
+  'The Status Quo',
+  'Rahul',
+  'Time Linearity',
+  'Limits',
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      wordIndex: 0,
+      wordPortion: '',
+    }
+  }
+
+  buildWord = () => {
+    const { wordIndex, wordPortion } = this.state;
+    const curWord = wordList[wordIndex];
+
+    if (wordPortion.length === curWord.length) {
+      setTimeout(this.destructWord, 1500);
+    } else {
+      this.setState({
+        wordPortion: wordPortion + curWord.charAt(wordPortion.length),
+      });
+
+      setTimeout(this.buildWord, 100);
+    }
+  }
+
+  destructWord = () => {
+    const { wordIndex, wordPortion } = this.state;
+    
+    if (wordPortion.length === 0) {
+      this.setState({
+        wordIndex: (wordIndex + 1) % wordList.length,
+      });
+      setTimeout(this.buildWord, 400);
+    } else {
+      this.setState({
+        wordPortion: wordPortion.slice(0, -1),
+      });
+      setTimeout(this.destructWord, 100);
+    }
+  }
+
+  componentDidMount() {
+    this.buildWord();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Engineering Open House</h1>
+          <h2>
+            Dare to Defy <span className="word">{this.state.wordPortion}</span>
+            <span className="type-cursor">|</span>
+          </h2>
+          <h3>March 8th - 9th, 2019</h3>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
