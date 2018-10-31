31a7be10ad93543dc35ebc042d464147940ca923
Jonathan Xu <jonathanj.xu97@gmail.com>
2018-10-30 22:37:23 -0500

    Splash Redesign (#5)
    * Style changes
    
    * Update traivs file
    
    * S3 config update
    
    * Site redesign

--- src/App.js
+++ src/App.js
@@ -1,76 +1,20 @@
 import React, { Component } from 'react';
-import logo from './assets/logo-white.png';
-import './App.scss';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const wordList = [
-  'The Status Quo',
-  'Limitations',
-  'Standards',
-  'Expectations',
-  'Constraints',
-  'Bounds',
-];
+import Nav from './components/Nav';
+import Home from './scenes/Home';
+
+import './styles.scss';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      wordIndex: Math.floor(Math.random() * Math.floor(wordList.length)),
-      wordPortion: '',
-    }
-  }
-
-  buildWord = () => {
-    const { wordIndex, wordPortion } = this.state;
-    const curWord = wordList[wordIndex];
-
-    if (wordPortion.length === curWord.length) {
-      setTimeout(this.destructWord, 1500);
-    } else {
-      this.setState({
-        wordPortion: wordPortion + curWord.charAt(wordPortion.length),
-      });
-
-      setTimeout(this.buildWord, 100);
-    }
-  }
-
-  destructWord = () => {
-    const { wordIndex, wordPortion } = this.state;
-
-    if (wordPortion.length === 0) {
-      this.setState({
-        wordIndex: (wordIndex + 1) % wordList.length,
-      });
-      setTimeout(this.buildWord, 800);
-    } else {
-      this.setState({
-        wordPortion: wordPortion.slice(0, -1),
-      });
-      setTimeout(this.destructWord, 100);
-    }
-  }
-
-  componentDidMount() {
-    this.buildWord();
-  }
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <div className="logo-wrapper">
-            <img src={logo} className="App-logo" alt="logo" />
-          </div>
-          <h1>Engineering Open House</h1>
-          <h2>
-            Dare to Defy <span className="word">{this.state.wordPortion}</span>
-            <span className="type-cursor">|</span>
-          </h2>
-          <h3>March 8th - 9th, 2019</h3>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Route path="/" exact component={Home} />
+        </div>
+      </Router>
     );
   }
 }
