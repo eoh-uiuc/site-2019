46550808900916cf3e7b3dec311fee652d7690c0
Jonathan Xu <jonathanj.xu97@gmail.com>
2019-02-25 21:14:27 -0600

    Finish committee

--- src/scenes/About/committee.js
+++ src/scenes/About/committee.js
@@ -1,6 +1,6 @@
 import Maddie from 'assets/headshots/Maddie.jpg';
-// import Emma from 'assets/headshots/.jpg';
-// import John from 'assets/headshots/.jpg';
+import Emma from 'assets/headshots/Emma.jpg';
+import John from 'assets/headshots/John.jpg';
 import Naomi from 'assets/headshots/Naomi.jpg';
 import Brandon from 'assets/headshots/Brandon.jpg';
 import Pratik from 'assets/headshots/Pratik.jpg';
@@ -10,20 +10,20 @@ import Sai from 'assets/headshots/Sai.jpg';
 import Timur from 'assets/headshots/Timur.jpg';
 import Rahul from 'assets/headshots/Rahul.jpg';
 import JJ from 'assets/headshots/JJ.jpg';
-// import Savni from 'assets/headshots/Savni.jpg';
+import Savni from 'assets/headshots/Savni.jpg';
 import Emily from 'assets/headshots/Emily.jpg';
 import Jane from 'assets/headshots/Jane.jpg';
 import Jenni from 'assets/headshots/Jenni.jpg';
 import Adi from 'assets/headshots/Adi.jpg';
 import Sujal from 'assets/headshots/Sujal.jpg';
-// import Andrew from 'assets/headshots/Andrew.jpg';
+import Andrew from 'assets/headshots/Andrew.jpg';
 import Michael from 'assets/headshots/Michael.jpg';
-// import Kourosh from 'assets/headshots/Kourosh.jpg';
+import Kourosh from 'assets/headshots/Kourosh.jpg';
 import Ali from 'assets/headshots/Ali.jpg';
 import Alice from 'assets/headshots/Alice.jpg';
 import Maneesh from 'assets/headshots/Maneesh.jpg';
 import Gonzalo from 'assets/headshots/Gonzalo.jpg';
-// import Arjun from 'assets/headshots/Arjun.jpg';
+import Arjun from 'assets/headshots/Arjun.jpg';
 import Dev from 'assets/headshots/Dev.jpg';
 
 const committee = [
@@ -34,20 +34,20 @@ const committee = [
     year: 'Senior',
     image: Maddie,
   },
-  // {
-  //   name: 'Emma Sementi',
-  //   position: 'Director of Exhibits',
-  //   major: 'Ag. and Bio Engineering',
-  //   year: 'Junior',
-  //   image: null,
-  // },
-  // {
-  //   name: 'John Choi',
-  //   position: 'Senior Coporate Director',
-  //   major: 'Computer Engineering',
-  //   year: 'Senior',
-  //   image: null,
-  // },
+  {
+    name: 'Emma Sementi',
+    position: 'Director of Exhibits',
+    major: 'Ag. and Bio Engineering',
+    year: 'Junior',
+    image: Emma,
+  },
+  {
+    name: 'John Choi',
+    position: 'Senior Coporate Director',
+    major: 'Computer Engineering',
+    year: 'Senior',
+    image: John,
+  },
   {
     name: 'Naomi Benson',
     position: 'Junior Coporate Director',
@@ -90,13 +90,13 @@ const committee = [
     year: 'Senior',
     image: Sai,
   },
-  // {
-  //   name: 'Savni Nagarkar',
-  //   position: 'Director of Vistor’s Information',
-  //   major: 'Computer Engineering',
-  //   year: 'Sophomore',
-  //   image: Savni,
-  // },
+  {
+    name: 'Savni Nagarkar',
+    position: 'Director of Vistor’s Information',
+    major: 'Computer Engineering',
+    year: 'Sophomore',
+    image: Savni,
+  },
   {
     name: 'Emily Roth ',
     position: 'Director of External Marketing',
@@ -132,13 +132,13 @@ const committee = [
     year: 'Junior',
     image: Sujal,
   },
-  // {
-  //   name: 'Andrew Gonsalves',
-  //   position: 'Director of MSDC',
-  //   major: 'Bioengineering',
-  //   year: 'Senior',
-  //   image: Andrew,
-  // },
+  {
+    name: 'Andrew Gonsalves',
+    position: 'Director of MSDC',
+    major: 'Bioengineering',
+    year: 'Senior',
+    image: Andrew,
+  },
   {
     name: 'Michael Gale',
     position: 'MRDC Director',
@@ -155,7 +155,7 @@ const committee = [
   },
   {
     name: 'Rahul Surti',
-    position: 'Lil Bitch',
+    position: 'Co-Director of Technology',
     major: 'Computer Engineering',
     year: 'Freshman',
     image: Rahul,
@@ -167,13 +167,13 @@ const committee = [
     year: 'Senior',
     image: JJ,
   },
-  // {
-  //   name: 'Kourosh Arasteh',
-  //   position: 'Director of Special Events',
-  //   major: 'Electrical Engineering',
-  //   year: 'Senior',
-  //   image: Kourosh,
-  // },
+  {
+    name: 'Kourosh Arasteh',
+    position: 'Director of Special Events',
+    major: 'Electrical Engineering',
+    year: 'Senior',
+    image: Kourosh,
+  },
   {
     name: 'Alixandra Ramos',
     position: 'Director of Advancement',
@@ -202,13 +202,13 @@ const committee = [
     year: 'Senior',
     image: Gonzalo,
   },
-  // {
-  //   name: 'Arjun Reddigari',
-  //   position: 'EFC Representative',
-  //   major: 'Computer Science',
-  //   year: 'Freshman',
-  //   image: Arjun,
-  // },
+  {
+    name: 'Arjun Reddigari',
+    position: 'EFC Representative',
+    major: 'Computer Science',
+    year: 'Freshman',
+    image: Arjun,
+  },
   {
     name: 'Dev Satpathy',
     position: 'EFC Representative',
